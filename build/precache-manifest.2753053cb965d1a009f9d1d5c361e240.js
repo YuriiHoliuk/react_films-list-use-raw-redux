d91334aa3ac74bbfec0b53f88518e83d56e88ad2
Vyacheslav Piskun <webdev.pv@gmail.com>
2019-10-03 08:33:30 +0300

    create build

---
+++ build/precache-manifest.2753053cb965d1a009f9d1d5c361e240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4132fa4a337ff6422246f6a31c3401db",
+    "url": "/react_films-list-use-raw-redux/index.html"
+  },
+  {
+    "revision": "d1b91513820c1990335a",
+    "url": "/react_films-list-use-raw-redux/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "c5b73f0a6b82061b56a8",
+    "url": "/react_films-list-use-raw-redux/static/css/main.e00c10dc.chunk.css"
+  },
+  {
+    "revision": "d1b91513820c1990335a",
+    "url": "/react_films-list-use-raw-redux/static/js/2.682e9ea8.chunk.js"
+  },
+  {
+    "revision": "c5b73f0a6b82061b56a8",
+    "url": "/react_films-list-use-raw-redux/static/js/main.7684a48e.chunk.js"
+  },
+  {
+    "revision": "7588ac357f07406e5d63",
+    "url": "/react_films-list-use-raw-redux/static/js/runtime~main.1e9a4e9f.js"
+  }
+]);
