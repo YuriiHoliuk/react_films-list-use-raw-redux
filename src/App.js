3caae1decf5d614f1161bba6a04e3995ed7ce737
Vyacheslav Piskun <webdev.pv@gmail.com>
2019-10-03 09:20:26 +0300

    change http to https

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { store, addNewFilm } from './store/index';
 import { FormField } from './components/FormField';
 import { FilmDetails } from './components/FilmDetails';
 
-const API_URL = 'http://www.omdbapi.com/?apikey=2f4a38c9&t=';
+const API_URL = 'https://www.omdbapi.com/?apikey=2f4a38c9&t=';
 
 store.dispatch(addNewFilm(...films));
 
