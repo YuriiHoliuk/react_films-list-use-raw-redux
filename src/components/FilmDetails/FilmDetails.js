30046292e58bedd1df514b99aab4926f66dc2577
Dmytro <voronok16@gmail.com>
2019-10-08 14:44:49 +0300

    solution

--- src/components/FilmDetails/FilmDetails.js
+++ src/components/FilmDetails/FilmDetails.js
@@ -1,50 +1,58 @@
 /* eslint-disable max-len */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { store } from '../../store';
 
 export class FilmDetails extends Component {
-  render() {
-    const {
+state = {
+  film: store.getState().films
+    .find(f => String(f.id) === this.props.match.params.id),
+}
+
+render() {
+  const {
+    film: {
       title,
       description,
       imgUrl,
       imdbUrl,
-    } = this.props;
-
-    return (
-      <div className="card">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <img
-              src={imgUrl}
-              alt="Film logo"
-            />
-          </figure>
+    },
+  } = this.state;
+  console.log(this.state.film)
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            src={imgUrl}
+            alt="Film logo"
+          />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img
+                src="https://images-na.ssl-images-amazon.com/images/I/315W5zxAnTL._SY355_.png"
+                alt="imdb"
+              />
+            </figure>
+          </div>
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+          </div>
         </div>
-        <div className="card-content">
-          <div className="media">
-            <div className="media-left">
-              <figure className="image is-48x48">
-                <img
-                  src="https://images-na.ssl-images-amazon.com/images/I/315W5zxAnTL._SY355_.png"
-                  alt="imdb"
-                />
-              </figure>
-            </div>
-            <div className="media-content">
-              <p className="title is-4">{title}</p>
-            </div>
-          </div>
 
-          <div className="content">
-            {description}
-            <br />
-            <a href={imdbUrl}>IMDB</a>
-          </div>
+        <div className="content">
+          {description}
+          <br />
+          <a href={imdbUrl}>IMDB</a>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
 
 FilmDetails.propTypes = {
--- src/components/FilmsList/FilmsList.js
+++ src/components/FilmsList/FilmsList.js
@@ -2,10 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './FilmsList.scss';
 import { FilmCard } from '../FilmCard';
+import { store } from '../../store';
 
-export const FilmsList = (props) => {
-  const { films } = props;
+class FilmsList extends React.Component {
+state = {
+  films: store.getState().films,
+}
 
+unsubscribe = null
+
+componentDidMount() {
+  this.unsubscribe = store
+    .subscribe(() => this.setState({ films: store.getState().films }));
+}
+
+componentWillUnmount() {
+  this.unsubscribe();
+}
+
+render() {
+  const { films } = this.state;
   return (
     <div className="films">
       {films.map(film => (
@@ -13,7 +29,8 @@ export const FilmsList = (props) => {
       ))}
     </div>
   );
-};
+}
+}
 
 FilmsList.propTypes = {
   films: PropTypes.arrayOf(PropTypes.shape({
@@ -27,3 +44,5 @@ FilmsList.propTypes = {
 FilmsList.defaultProps = {
   films: [],
 };
+
+export default FilmsList;
